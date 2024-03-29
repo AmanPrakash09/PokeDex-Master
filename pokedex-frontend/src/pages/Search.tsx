2a81cb88e90ef48c669c43e1d01c15fe1bdcbf9e
Valentino Jaber <danial27@student.ubc.ca>
2024-03-28 17:37:32 -0700

    Basic UI for Searching

--- pokedex-frontend/src/pages/Search.tsx
+++ pokedex-frontend/src/pages/Search.tsx
@@ -6,7 +6,61 @@ function Search() {
   return (
     <>
       <div className="search-container">
-        yo
+        <div className="logo-container">
+          <a href="http://localhost:3000">
+            <img src={appLogo} className="logo" alt="Project logo" />
+          </a>
+        </div>
+
+        <div> 
+          <h1>PokeDex Master - Search Pokemon</h1>
+          <h4>Search Pokemon By Name or By Using Filters</h4>
+          <div style={{ display: 'flex', alignItems: 'center'}}>
+            <div className="search-bar">
+              <input type="text" placeholder="Search Pokemon" />
+              <button>Search</button>
+            </div>
+            <div className="filter-container">
+              <div className="dropdown">
+                <label htmlFor="type">Type:</label>
+                <select id="type">
+                  <option value="">Any</option>
+                  <option value="grass">Grass</option>
+                  <option value="fire">Fire</option>
+                  <option value="water">Water</option>
+                  <option value="poison">Poison</option>
+                  <option value="flying">Flying</option>
+                  <option value="bug">Bug</option>
+                  <option value="normal">Normal</option>
+                  <option value="electric">Electric</option>
+                  <option value="ground">Ground</option>
+                  <option value="dark">Dark</option>
+                  <option value="steel">Steel</option>
+                  <option value="fighting">Fighting</option>
+                  <option value="psychic">Psychic</option>
+                  <option value="ice">Ice</option>
+                  <option value="water">Water</option>
+                  <option value="ghost">Ghost</option>
+                  <option value="fairy">Fairy</option>
+                  <option value="rock">Rock</option>
+                </select>
+              </div>
+              <div className="dropdown">
+                <label htmlFor="generation">Generation:</label>
+                <select id="generation">
+                  <option value="">Any</option>
+                  <option value="1">Generation 1</option>
+                  <option value="3">Generation 3</option>
+                </select>
+              </div>
+              <div className="checkbox">
+                <input type="checkbox" id="caught" />
+                <label htmlFor="caught">Caught</label>
+              </div>
+              <button className="search-button">Search</button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
