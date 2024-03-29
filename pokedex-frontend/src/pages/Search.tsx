6f1fcf7362a9d015d0e61662c40a18023a427123
Valentino Jaber <danial27@student.ubc.ca>
2024-03-28 18:09:08 -0700

    Created Search Results UI

--- pokedex-frontend/src/pages/Search.tsx
+++ pokedex-frontend/src/pages/Search.tsx
@@ -3,6 +3,24 @@ import appLogo from '../assets/PokedexLogo.png'
 import './Search.css'
 
 function Search() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      // Make an API call to fetch the matching Pokemon data based on the search query
+      const response = await fetch(`http://127.0.0.1:8000/pokemon/search?name=${searchQuery}`);
+      if (response.ok) {
+        const data = await response.json();
+        setSearchResults(data); // Set the search results in state
+      } else {
+        console.error('Failed to fetch search results');
+      }
+    } catch (error) {
+      console.error('Error occurred while fetching search results:', error);
+    }
+  };
+
   return (
     <>
       <div className="search-container">
@@ -17,9 +35,15 @@ function Search() {
           <h4>Search Pokemon By Name or By Using Filters</h4>
           <div style={{ display: 'flex', alignItems: 'center'}}>
             <div className="search-bar">
-              <input type="text" placeholder="Search Pokemon" />
-              <button>Search</button>
+              <input 
+                type="text" 
+                placeholder="Search Pokemon" 
+                value={searchQuery} 
+                onChange={(e) => setSearchQuery(e.target.value)} 
+              />
+              <button onClick={handleSearch}>Search</button>
             </div>
+            
             <div className="filter-container">
               <div className="dropdown">
                 <label htmlFor="type">Type:</label>
@@ -60,6 +84,28 @@ function Search() {
               <button className="search-button">Search</button>
             </div>
           </div>
+
+          <div className="search-results">
+          <h2>Search Results</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Pokemon Name</th>
+                <th>Type</th>
+                <th>Egg Group</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* {searchResults.map((pokemon) => (
+                <tr key={pokemon.PokemonName}>
+                  <td>{pokemon.PokemonName}</td>
+                  <td>{pokemon.Type}</td>
+                  <td>{pokemon.EggGroup}</td>
+                </tr>
+              ))} */}
+            </tbody>
+          </table>
+        </div>
         </div>
       </div>
     </>
